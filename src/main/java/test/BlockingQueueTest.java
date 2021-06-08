47cb89e6a234154ad7347086c9e86753dc84439d
火火 <1047984367@qq.com>
2021-06-08 22:43:40 +0800

    放置死锁

--- src/main/java/test/BlockingQueueTest.java
+++ src/main/java/test/BlockingQueueTest.java
@@ -15,10 +15,14 @@ public class BlockingQueueTest {
         BlockingQueue<Integer> queue;
         //queue = new ArrayBlockingQueue<Integer>(10);
         //queue = new LinkedBlockingQueue<Integer>();
+        //双向队列 提供first、last元素操作方法
         //queue = new LinkedBlockingDeque<>();
+        //内部是有序的树型堆（有点像二叉树） 头小 尾大 增加数据 从头开始挨个比较下沉 删除数据 从尾开始挨个比较上升
         //queue = new PriorityBlockingQueue<>();
+        //
         //queue = new LinkedTransferQueue<Integer>();
         queue = new SynchronousQueue<>();
+        //内部 PriorityQueue 每次取数据 更新最近的一条过期时间
         //queue = new DelayQueue<>();
 
         // Producer
