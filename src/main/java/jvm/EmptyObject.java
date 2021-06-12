fb15e9227939c7a008e58cfde48cb04a3629bf01
火火 <1047984367@qq.com>
2021-06-12 19:17:09 +0800

    完善笔记

--- src/main/java/jvm/EmptyObject.java
+++ src/main/java/jvm/EmptyObject.java
@@ -9,19 +9,23 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class EmptyObject {
 
-
-
-
+    // 注入Instrumentation流程（jdk本身的对象 不可以new）
+    // 1 把InstrumentationAgent 编译成class 文件
+    // 2 打成jar 放入.MF 指定注入的对象
+    // 3 运行本方法 需要指定 InstrumentationAgent jar地址 --javaagent:/home/*/InstrumentationAgent.jar
     //-XX:-UseCompressedClassPointers
     //-XX:-UseCompressedOops
     public static void main(String[] argv){
+        //16
         System.out.println(InstrumentationAgent.getObjectSize(new Object()));
+        //16
         System.out.println(InstrumentationAgent.getObjectSize(new int[0]));
+        //24
         System.out.println(InstrumentationAgent.getObjectSize(""));
-
-
+        //16
         Integer y = 1;
         System.out.println(InstrumentationAgent.getObjectSize(y));
+        //16
         int x = 1;
         System.out.println(InstrumentationAgent.getObjectSize(x));
     }
